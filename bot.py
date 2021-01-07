1db541d270a9ae0aa017db2b14cb6b121883fe27
mocomaminecraft <mocomaminecraft@gmail.com>
2021-01-07 12:48:30 +0100

    Done

--- bot.py
+++ bot.py
@@ -1,7 +1,67 @@
 from repeated_task import RepeatedTask
 from dateutil import parser
 import telegram
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
 import secret
 
-updater = Updater(token=secret.token)
+tasks = []
+
+def start(updater, context):
+    context.bot.send_message(chat_id=updater.effective_chat.id, text="Bot ready to wish a really good morning to everyone! \u263A")
+    context.bot.send_message(chat_id=updater.effective_chat.id, text="You can use the /help command to learn how to configure me!")
+
+def help(updater, context):
+    help_text = ""
+    try:
+        with open("help.txt", "r") as f:
+            help_text = f.read()
+    except:
+        context.bot.send_message(chat_id=updater.effective_chat.id, text="Whoops! There seems to be a problem! contact @Mocoma for help")
+
+    if help_text:
+        context.bot.send_message(chat_id=updater.effective_chat.id, text=help_text)
+
+def get_task(task_id):
+    id_tasks = [task for task in tasks if task[0] == task_id]
+    if id_tasks:
+        return id_tasks[0][1]
+    else:
+        task = RepeatedTask()
+        tasks.append((task_id, task))
+        return task
+
+def update_task_time(updater, context):
+    task = get_task(updater.effective_chat.id)
+
+    time = None
+    try:
+        time = parser.parse(" ".join(context.args))
+    except ParserException:
+        context.bot.send_message(chat_id=updater.effective_chat.id, text="It seems the format you introduced is not valid! try again or use /help if you need more info")
+
+    if time:
+        task.time_of_repetition = time
+        task.restart()
+
+        context.bot.send_message(chat_id=updater.effective_chat.id, text="Done! You will be good morning-ed!")
+
+def update_task_text(updater, context):
+    task = get_task(updater.effective_chat.id)
+
+    text = " ".join(context.args)
+    if text:
+        task.task = lambda: context.bot.send_message(chat_id=updater.effective_chat.id, text=text)
+        if not task.is_alive():
+            task.restart()
+
+        context.bot.send_message(chat_id=updater.effective_chat.id, text="Done! Your message has been set!")
+
+updater = Updater(token=secret.token, use_context=True)
+dispatcher = updater.dispatcher
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("help", help))
+dispatcher.add_handler(CommandHandler("set_time", update_task_time))
+dispatcher.add_handler(CommandHandler("set_message", update_task_text))
+
+updater.start_polling()
--- repeated_task.py
+++ repeated_task.py
@@ -2,16 +2,37 @@ from time import sleep
 from datetime import datetime, timedelta
 from threading import Thread
 
-class RepeatedTask(Thread):
-    def __init__(self, task, time_of_repetition):
+class RepeatedTask():
+    def __init__(self, task=None, time_of_repetition=None):
         self.task = task
         self.time_of_repetition = time_of_repetition
+        self.thread = None
+    
+    def restart(self):
+        if self.thread: self.thread.stop()
+        self.thread = StoppableThread(self.task, self.time_of_repetition) 
+        print(self.time_of_repetition)
+        self.thread.start()
+
+    def is_alive(self):
+        return self.thread and self.thread.is_alive()
+
+class StoppableThread(Thread):
+    def __init__(self, task=None, time_of_repetition=None):
+        super().__init__()
+        self.task = task
+        self.time_of_repetition = time_of_repetition
+        self.running = True
+
+    def stop(self):
+        self.running = False
 
     def run(self):
-        while True:
+        while self.running and self.task and self.time_of_repetition:
             now = datetime.now()
             next_timestamp = self.time_of_repetition.replace(day=now.day, month=now.month, year=now.year)
             if next_timestamp < now: next_timestamp += timedelta(days=1)
 
             sleep((next_timestamp - now).total_seconds())
-            self.task()
+            if self.running:
+                self.task()
